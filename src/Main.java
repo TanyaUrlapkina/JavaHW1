c81848fdfa2e8fbe0a2437a6fe00089eef911841
TanyaUrlapkina <write2me92@mail.ru>
2021-10-03 22:50:26 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int check = 2_000_000_000;
         int transfer = 500_000_000;
-        int total = transfer + check;
+        int total = check + transfer;
         System.out.println(total);
     }
 }
